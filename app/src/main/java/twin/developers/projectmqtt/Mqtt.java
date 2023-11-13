7e469c254f798c9f661ef6afba3de75b1cce262f
Manuel <manuelmerinopiceros@gmail.com>
2023-11-13 16:02:48 -0300

    v1

--- app/src/main/java/twin/developers/projectmqtt/Mqtt.java
+++ app/src/main/java/twin/developers/projectmqtt/Mqtt.java
@@ -15,9 +15,9 @@ import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 
 public class Mqtt {
     private static final String TAG = "MQTT";
-    //private static final String MQTT_SERVER = "tcp://broker.emqx.io:1883";
-    private static final String MQTT_SERVER = "tcp://10.51.0.57:1883";
-    private static final String CLIENT_ID = "AndroidSample123123123";
+    private static final String MQTT_SERVER = "tcp://broker.emqx.io:1883";
+    //private static final String MQTT_SERVER = "tcp://127.0.0.1:1883";
+    private static final String CLIENT_ID = "AndroidSample12312312312312312";
     private static final String TOPIC = "iot/lab/test";
     private static String MESSAGE = "";
     private static final int QOS = 2;
